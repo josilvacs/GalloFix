6bc5dd7ed909722d51d4e2c50802782590fd0390
Joany Camily da Silva <joanycamily23@gmail.com>
2023-06-05 11:34:19 -0300

    criação do serviço de email

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 using System.Net.Mail;
 using System.Security.Claims;
+using System.Text;
 using GalloFlix.DataTransferObjects;
 using GalloFlix.Models;
+using GalloFlix.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace GalloFlix.Controllers;
 
@@ -14,15 +17,24 @@ public class AccountController : Controller
     private readonly ILogger<AccountController> _logger;
     private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
+    private readonly IUserStore<AppUser> _userStore;
+    private readonly IUserEmailStore<AppUser> _emailStore;
+    private readonly IEmailSender _emailSender;
 
     public AccountController(
-        ILogger<AccountController> logger,
-        SignInManager<AppUser> signInManager,
-        UserManager<AppUser> userManager)
+        ILogger<AccountController> logger, 
+        SignInManager<AppUser> signInManager, 
+        UserManager<AppUser> userManager,
+        IUserStore<AppUser> userStore,
+        IEmailSender emailSender
+    )
     {
         _logger = logger;
         _signInManager = signInManager;
         _userManager = userManager;
+        _userStore = userStore;
+        _emailStore = (IUserEmailStore<AppUser>)_userStore;
+        _emailSender = emailSender;
     }
 
     public IActionResult Index()
@@ -53,7 +65,6 @@ public class AccountController : Controller
                 if (user != null)
                     userName = user.UserName;
             }
-
             var result = await _signInManager.PasswordSignInAsync(
                 userName, login.Password, login.RememberMe, true
             );
@@ -64,23 +75,21 @@ public class AccountController : Controller
             }
             if (result.IsLockedOut)
             {
-                _logger.LogWarning($"Usuário { login.Email } acessou o bloqueado");
+                _logger.LogWarning($"Usuário { login.Email } está bloqueado");
                 return RedirectToAction("Lockout");
             }
-            ModelState.AddModelError("login","Usuário e/ou Senha Inválidos!!!");
-            
+            ModelState.AddModelError("login", "Usuário e/ou Senha Inválidos!!!");
         }
         return View(login);
     }
 
     [HttpPost]
-    public async Task<IActionResult> logout()
+    public async Task<IActionResult> Logout()
     {
         _logger.LogInformation($"Usuário {ClaimTypes.Email} fez logoff");
         await _signInManager.SignOutAsync();
         return RedirectToAction("Index", "Home");
     }
-
 
     [HttpGet]
     [AllowAnonymous]
@@ -89,8 +98,40 @@ public class AccountController : Controller
         return View();
     }
 
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register(RegisterDto register)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = Activator.CreateInstance<AppUser>();
 
+            user.Name = register.Name;
+            user.DateOfBirth = register.DateOfBirth;
+            user.Email = register.Email;
 
+            await _userStore.SetUserNameAsync(
+                user, register.Email, CancellationToken.None);
+            await _emailStore.SetEmailAsync (
+                user, register.Email, CancellationToken.None);
+            
+            var result = await _userManager.CreateAsync(user, register.Password);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Novo usuário registrado com o email {user.Email}");
+
+                var userId = await _userManager.GetUserIdAsync(user);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                var callbackUrl = Url.Action(
+                    "ConfirmeEmail", "Account", new { userId = userId, code },
+                    protocol: Request.Scheme
+                );
+            }
+        }
+        return View(register);
+    }
 
     private bool IsValidEmail(string email)
     {
@@ -104,4 +145,5 @@ public class AccountController : Controller
             return false;
         }
     }
+
 }
